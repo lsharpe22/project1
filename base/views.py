d90da8f698388f9642aa18d6b5ad2b39a7859667
Lauren <lsharp22@charlotte.edu>
2025-02-03 14:14:30 -0500

    Database and Admin Panel

--- base/views.py
+++ base/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+    {'id':1, 'name' : 'Lets learn python!'},
+    {'id':2, 'name' : 'Design with me'},
+    {'id':3, 'name' : 'Frontend developers'}
+]
 # Create your views here.
 def home(request):
-    return HttpResponse('Home page')
-def room(request):
-    return HttpResponse('ROOM')
+    context ={'rooms': rooms}
+    return render(request, 'base/home.html',context)
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i ['id']== int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
 
 
